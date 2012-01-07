63c1f2cf541e09c0b772af276422f3e9642833de
Paul Osborne <osbpau@gmail.com>
2012-01-07 13:21:01 -0600

    ch4: Basic working version of copy (without holes) working

--- ch4/cpholes.c
+++ ch4/cpholes.c
@@ -5,9 +5,63 @@
  */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
 
-int main()
+#ifndef BUF_SIZE /* Allow "cc -D" to override */
+#define BUF_SIZE 1024
+#endif
+
+void print_usage()
 {
-	printf("TODO!\n");
+	printf("Usage: ./prog_cpholes <src> <dst>\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int inputFd, outputFd, openFlags;
+	mode_t filePerms;
+	ssize_t numRead;
+	char buf[BUF_SIZE];
+
+	/* parse argv
+	 * ./prog_cpholes src dst
+	 */
+	if (argc != 3 || strcmp(argv[1], "--help") == 0) {
+		print_usage();
+		return 1;
+	}
+	
+	/* Open input and output files */
+	openFlags = (O_CREAT | O_WRONLY | O_TRUNC);
+	filePerms = (S_IRUSR | S_IWUSR | S_IRGRP |
+		     S_IROTH | S_IWOTH);
+	
+	inputFd = open(argv[1], O_RDONLY);
+	if (inputFd < 0) {
+		printf("Failed to open input file: %s\n", strerror(inputFd));
+		print_usage();
+		return 1;
+	}
+	
+	outputFd = open(argv[2], openFlags, filePerms);
+	if (outputFd < 0) {
+		printf("Failed to open output file: %s\n", strerror(inputFd));
+		print_usage();
+		return 1;
+	}
+	
+	/* do the copy operation */
+	while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0) {
+		if (write(outputFd, buf, numRead) != numRead) {
+			printf("Failed to write full buffer to output file!\n");
+			return 1;
+		}
+	}
+
+	/* close fds */
+	close(inputFd);
+	close(outputFd);
+
 	return 0;
 }
